79564973186bfc5241b8d2bc22aba5d30d87011c
Saloni0412 <pshashikantb@gmail.com>
2023-07-19 13:01:52 -0400

    this commit adds code to accept the content and add it to database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,26 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT to the Database');
+  const jateDB = await openDB('jate', 1);
+  const tx = jateDB.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({id: 1, content: content});
+  const result = await request;
+  console.log('data saved to the database successfully!', result.content);
+
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
+export const getDb = async () => {
+console.log('GET from the database');
+const jateDB = await openDB('jate', 1);
+const tx = jateDB.transaction('jate', 'readonly');
+const store = tx.objectStore('jate');
+const request = store.getAll();
+const result = await request;
+console.log('result.value', result.content);
+return result;
+}
 initdb();
